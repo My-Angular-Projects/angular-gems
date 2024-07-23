eede3acfd261b313f0798b5e9758c37e33b11286
Muso Verda <gearmobile@gmail.com>
2024-07-24 00:54:09 +0300

    continue

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Todo } from '../../types/todo.interface';
 import { User } from '../../types/user.interface';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'gm-todos',
@@ -12,8 +13,12 @@ import { User } from '../../types/user.interface';
 export class TodosComponent {
   public readonly pageTitle = 'Team Members and Tasks';
 
+  private readonly todoService = inject(TodoService);
+
   public readonly todosForMember = signal<Todo[]>([]);
   public readonly members = signal<User[]>([]);
+
+  public readonly errorMessage = this.todoService.errorMessage;
 
   public onSelectedMember(memberId: EventTarget | null): void {
     //
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { catchError, delay, map, Observable, of, takeUntil } from 'rxjs';
 import { Todo } from '../types/todo.interface';
 import { errorHandler } from '../helpers/error-handler.helper';
 
@@ -9,9 +10,20 @@ import { errorHandler } from '../helpers/error-handler.helper';
 })
 export class TodoService {
   private readonly http = inject(HttpClient);
+  private readonly destroyRef = inject(DestroyRef);
   private readonly url = 'https://jsonplaceholder.typicode.com/todos';
 
   public errorMessage = signal<string>('');
+  public todos = signal<Todo[]>([]);
+
+  public setUserId(userId: number): void {
+    // Clear the todos from the prior selection
+    this.todos.set([]);
+
+    this.getTodos(userId)
+      .pipe(delay(2000), takeUntilDestroyed(this.destroyRef))
+      .subscribe((todos: Todo[]) => this.todos.set(todos));
+  }
 
   private getTodos(userId: number): Observable<Todo[]> {
     const url = `${this.url}?userId=${userId}`;
